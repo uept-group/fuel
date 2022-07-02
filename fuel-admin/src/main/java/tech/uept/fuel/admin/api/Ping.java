c55faa34a01e735a3646b5fce2a6893b1a1c72f3
xhrg <634789257@qq.com>
2022-07-02 20:23:03 +0800

    ping

---
+++ fuel-admin/src/main/java/tech/uept/fuel/admin/api/Ping.java
@@ -0,0 +1,24 @@
+package tech.uept.fuel.admin.api;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ping implements CommandLineRunner {
+
+    private String pong;
+
+    @RequestMapping("/ping")
+    public String pong() {
+        return pong;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        pong = "pong! start time is " + DateFormat.getDateTimeInstance().format(new Date());
+    }
+}
