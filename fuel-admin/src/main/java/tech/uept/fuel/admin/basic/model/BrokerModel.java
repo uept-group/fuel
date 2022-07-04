21a66cf35b7a6d479b97bdb367114b5b7137e636
xhrg <634789257@qq.com>
2022-07-04 11:02:28 +0800

    broker

--- fuel-admin/src/main/java/tech/uept/fuel/admin/basic/model/BrokerModel.java
+++ fuel-admin/src/main/java/tech/uept/fuel/admin/basic/model/BrokerModel.java
@@ -5,7 +5,7 @@ public class BrokerModel {
     // 名称
     private String name;
 
-    // 编号
+    // 编号，broker编号，如果是0则是主节点，如果是1，2，3，4则都是从节点。一般情况下，都是一个0，一个1，也就是1主1从
     private long no;
 
     private String address;
