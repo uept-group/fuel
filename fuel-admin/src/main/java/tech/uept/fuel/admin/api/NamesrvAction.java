9109ffdf26f6f327164c60df34445da45b92e127
xhrg <634789257@qq.com>
2022-07-13 20:58:05 +0800

    namesrv update

--- fuel-admin/src/main/java/tech/uept/fuel/admin/api/NamesrvAction.java
+++ fuel-admin/src/main/java/tech/uept/fuel/admin/api/NamesrvAction.java
@@ -2,6 +2,7 @@ package tech.uept.fuel.admin.api;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +17,17 @@ public class NamesrvAction {
     private NamesrvService namesrvService;
 
     @RequestMapping("/insert")
-    public void insert(NamesrvModel namesrvModel) {
+    public void insert(@RequestBody NamesrvModel namesrvModel) {
         namesrvService.insert(namesrvModel);
     }
 
     @RequestMapping("/update")
-    public void update(NamesrvModel namesrvModel) {
+    public void update(@RequestBody NamesrvModel namesrvModel) {
         namesrvService.update(namesrvModel);
     }
 
     @RequestMapping("/delete")
-    public void delete(NamesrvModel namesrvModel) {
+    public void delete(@RequestBody NamesrvModel namesrvModel) {
         namesrvService.delete(namesrvModel.getId());
     }
 
