3869cae1fc5b714636689fd28485dd688d4d3135
xhrg <634789257@qq.com>
2022-07-13 17:54:31 +0800

    log fix

--- fuel-admin/src/main/java/tech/uept/fuel/admin/FuelApplication.java
+++ fuel-admin/src/main/java/tech/uept/fuel/admin/FuelApplication.java
@@ -1,5 +1,6 @@
 package tech.uept.fuel.admin;
 
+import org.apache.rocketmq.client.log.ClientLogger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("tech.uept.fuel.admin.basic.mapper")
 public class FuelApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FuelApplication.class, args);
+    }
+
+    static {
+        System.setProperty(ClientLogger.CLIENT_LOG_USESLF4J, "true");
     }
 }
